c79dbb4d4bba90594718147860f69eeec4c3314f
M Nasir Khan <120434670+mnasirkhan97@users.noreply.github.com>
2025-05-06 23:29:09 +0500

    Using cls

--- app.py
+++ app.py
@@ -11,3 +11,22 @@ class Student:
 # Example usage:
 student1 = Student("Ali", 85)
 student1.display()
+
+
+#  2. Using cls
+class Counter:
+    count = 0  # Class variable to keep track of object count
+
+    def __init__(self):
+        Counter.count += 1
+
+    @classmethod
+    def show_count(cls):
+        print(f"Total objects created: {cls.count}")
+
+# Example usage:
+obj1 = Counter()
+obj2 = Counter()
+obj3 = Counter()
+
+Counter.show_count()
